d749897a17c9d559f22cde3348b4294c51a6bc7e
Cydev2306 <charlier.cy@gmail.com>
2017-02-23 22:44:25 +0100

    get started
    get started
    Structure de l’api Connect & drink

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+const user = require('./app/user/userRoutes');
+
+// Middlewares
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log/access.log'), { flags: 'a' });
+
+app.use(morgan('combined', { stream: accessLogStream }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/user', user);
+
+// using arrow syntax
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
+});
+app.listen(port);
